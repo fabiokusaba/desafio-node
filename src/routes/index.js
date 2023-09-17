19df06f2fb95e9fbcdb2dd80aec27f079fce0fe5
Fabio Kusaba <fabinhokb44@gmail.com>
2023-09-17 10:05:48 -0300

    feat: adicionado movie_tags

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,10 +2,12 @@ const { Router } = require('express')
 
 const usersRouter = require('./users.routes')
 const movieNotesRouter = require('./movienotes.routes')
+const movieTagsRouter = require('./movietags.routes')
 
 const routes = Router()
 
 routes.use('/users', usersRouter)
 routes.use('/movie_notes', movieNotesRouter)
+routes.use('/movie_tags', movieTagsRouter)
 
 module.exports = routes
